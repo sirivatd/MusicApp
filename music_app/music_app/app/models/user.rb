1e029c47bb29dd9c5b30ca785ad6327932ab6a1f
sirivat <dsirivat17@gmail.com>
2018-09-13 11:33:30 -0700

    add users routes

---
+++ music_app/music_app/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: 'cant be blank'}
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def reset_session_token!
+    self.sesion_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
---
+++ music_app/music_app/db/migrate/20180913181500_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, uniqueness: true
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, uniqueness: true
+
+      t.timestamps
+    end
+    add_index :users, :email
+  end
+end
